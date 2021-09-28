57df8498569f191c56449936d5ea4f82938c4b9f
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-09-28 22:53:05 +0300

    Lesson6 (вернул обратно в setting аутинф)

--- rest_django/urls.py
+++ rest_django/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from userapp.views import UserModelViewSet
 from todo.views import ProjectModelViewSet, TodoModelViewSet
-# from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet, basename='users')
@@ -13,6 +13,6 @@ router.register('todos', TodoModelViewSet, basename='todos')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path('api-token-auth/', obtain_auth_token),
+    path('api-token-auth/', obtain_auth_token),
     path('api/', include(router.urls)),
 ]
