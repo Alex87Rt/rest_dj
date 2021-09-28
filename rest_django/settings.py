5afb49a941feaeacd6f34a12226a25d2d2166c11
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-09-28 22:33:50 +0300

    Lesson5

--- rest_django/settings.py
+++ rest_django/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = 'django-insecure-hniib_*5b!c8gdnjwjae+_x4-zql)^qr=n)mr&h&u^$hja#m8b
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -38,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'corsheaders',
     'userapp',
     'todo',
@@ -135,21 +133,11 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions',
-    ],
-
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-
 }
-
-CORS_ALLOWED_ORIGINS = [
-   "http://localhost:3000",
-]
+#
+# CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:8000",
+# ]
