0f884cb18b5f7eec4ff0c23de16b48c2a46f1535
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-09-29 22:54:52 +0300

    Lesson7

--- rest_django/settings.py
+++ rest_django/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
 }
-#
-# CORS_ALLOWED_ORIGINS = [
-#    "http://localhost:8000",
-# ]
+
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:3000",
+]
